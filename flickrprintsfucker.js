2d06f0089c520cca1761edcf76918c7bd6637dc0
Tobias <tobias@moeritz.cc>
2019-08-31 17:55:14 +0200

    cool

---
+++ flickrprintsfucker.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         Flickr Prints Fucker
+// @namespace    https://github.com/tobimori/
+// @version      1.0
+// @description  Removes Flickr Prints advertising button in navbar and picture viewer
+// @author       tobimori
+// @downloadURL  https://raw.githubusercontent.com/tobimori/flickrprints-fucker/master/flickrprintsfucker.js
+// @updateURL    https://raw.githubusercontent.com/tobimori/flickrprints-fucker/master/flickrprintsfucker.js
+// @match        https://*.flickr.com/*
+// @require 	 https://code.jquery.com/jquery-3.3.1.min.js
+// ==/UserScript==
+
+var oldUrl = "none";
+
+document.getElementsByClassName("gn-create")[0].remove();
+
+var observer = new MutationObserver(function (mutations, me) {
+    if (document.getElementsByClassName("view print-button-view")[0] !== undefined) {
+        while (document.getElementsByClassName("view print-button-view")[0]) {
+            document.getElementsByClassName("view print-button-view")[0].remove();
+        };
+        return;
+    };
+});
+
+var navbar = new MutationObserver(function (mutations, me) {
+	if (oldUrl == document.URL) return;
+    if (document.getElementsByClassName("gn-create")[0] !== undefined) document.getElementsByClassName("gn-create")[0].remove();
+});
+
+navbar.observe($('html')[0], {
+	childList: false,
+	attributes: true,
+});
+
+observer.observe(document, {
+  childList: true,
+  subtree: true
+});
